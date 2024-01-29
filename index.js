36cfa2ef3f4cc7a96fb4ddff93a27d8248c89910
Yuri <+>
2024-01-29 00:55:46 -0500

    Completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 6.2
+let num2 = 10
+const multiply = num1 * num2
 
-// Write your code here
+let random = 7
+Math.floor(Math.random() * 9) + 7;
+
+let num3 = 8
+let num4 = 32
+const mod = num4 / num3
+
+let max = 20
+Math.max(0, 5, 10, 15, 20);
